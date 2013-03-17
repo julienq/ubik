4baa2b7fae9e4589262f39305d50a696f0666630
Julien Quint <julien@igel.co.jp>
2013-03-17 22:39:51 +0900

    Merging status updates from followed users

--- server/app.js
+++ server/app.js
@@ -29,17 +29,35 @@ function html(params, head, body) {
       flexo.$body(body));
 }
 
+function check_uid(uid, next, notfound, found) {
+  redis.SISMEMBER("users", uid, function (err, reply) {
+    if (err) {
+      next(err);
+    } else if (reply === 0) {
+      notfound("No user %0".fmt(uid));
+    } else {
+      found(uid);
+    }
+  });
+}
+
+// Zip results of ZRANGE with scores, returning a list of (value, score) pairs
+function ziprange(range) {
+  for (var i = 0, z = [], n = range.length - 1; i < n; i += 2) {
+    z.push([range[i], range[i + 1]]);
+  }
+  return z;
+}
+
 var app = express();
 
 app.use(express.bodyParser());
 app.use("/static", express.static(__dirname + "/static"));
 
-// A user is defined by:
-// users = set of user ids
-// user:<uid> = { first, last, avatar? }
-// user:<uid>:follows = set of followed uids
-// user:<uid>:followers = set of follower uids
-// user:<uid>:status = sorted set of status updates (just text)
+
+// Add a new user:
+// curl -X PUT -d '{"first":...,"last":...}' -H "Content-type: application/json"
+//   http://127.0.0.1:7000/user/<uid>
 app.put("/user/:uid", function (req, res, next) {
   var m = redis.multi();
   m.SADD("users", req.params.uid);
@@ -50,7 +68,6 @@ app.put("/user/:uid", function (req, res, next) {
       m.HSET(key, param, req.body[param]);
     }
   });
-  m.ZADD(key + ":status", Date.now(), "I just joined Ubik. Whoa!");
   m.exec(function (err) {
     if (err) {
       next(err);
@@ -60,45 +77,118 @@ app.put("/user/:uid", function (req, res, next) {
   });
 });
 
+// Status update
+// curl -X PUT -d '{"msg":...}' -H "Content-type: application/json"
+//   http://127.0.0.1:7000/user/<uid>/status
 app.put("/user/:uid/status", function (req, res, next) {
-  redis.SISMEMBER("users", req.params.uid, function (err, reply) {
-    if (err) {
-      next(err);
-    } else if (reply === 0) {
-      next("No user %0".fmt(req.params.uid));
-    } else {
-      var date = req.body.date || Date.now();
-      redis.ZADD("user:%0:status".fmt(req.params.uid), date, req.body.msg,
-        function (err, reply) {
+  check_uid(req.params.uid, next, next, function (uid) {
+    var date = req.body.date || Date.now();
+    redis.INCR("counter", function (err, reply) {
+      if (err) {
+        next(err);
+      } else {
+        var id = reply.toString(36);
+        redis.multi()
+          .ZADD("user:%0:status".fmt(uid), date, id)
+          .HSET("status:%0".fmt(id), "id", id)
+          .HSET("status:%0".fmt(id), "date", date)
+          .HSET("status:%0".fmt(id), "from", uid)
+          .HSET("status:%0".fmt(id), "msg", req.body.msg)
+        .exec(function (err) {
           if (err) {
             next(err);
           } else {
             res.send(201);
           }
         });
-    }
+      }
+    });
+  });
+});
+
+// Follow someone
+app.put("/user/:srcid/follow/:destid", function (req, res, next) {
+  check_uid(req.params.srcid, next, next, function (srcid) {
+    check_uid(req.params.destid, next, next, function (destid) {
+      var now = Date.now();
+      redis.multi()
+        .ZADD("user:%0:following".fmt(srcid), now, destid)
+        .ZADD("user:%0:followers".fmt(destid), now, srcid)
+        .exec(function (err) {
+          if (err) {
+            next(err);
+          } else {
+            res.send(201);
+          }
+        });
+    });
   });
 });
 
 app.get("/user/:uid", function (req, res, next) {
-  redis.HGETALL("user:%0".fmt(req.params.uid), function (err, reply) {
+  redis.multi()
+    .HGETALL("user:%0".fmt(req.params.uid))
+    .ZRANGE("user:%0:following".fmt(req.params.uid), 0, -1)
+    .ZRANGE("user:%0:followers".fmt(req.params.uid), 0, -1)
+  .exec(function (err, replies) {
     if (err) {
       next(err);
-    } else if (!reply) {
+    } else if (!replies[0]) {
       res.send(404);
     } else {
-      var user = "%0 %1".fmt(reply.first, reply.last);
-      redis.ZREVRANGE("user:%0:status".fmt(req.params.uid), 0, -1, "WITHSCORES",
-        function (err, reply) {
-          for (var i = 0, updates = "", n = reply.length; i < n; i += 2) {
-            var msg = reply[i];
-            var date = new Date(parseFloat(reply[i + 1]));
-            updates += flexo.$p("%0 said: “%1” (on %2)".fmt(user, msg, date));
-          }
-          res.send(200, html({ title: "Ubik | %0".fmt(reply.uid) },
-              flexo.$link({ rel: "stylesheet", href: "/static/ubik.css" }),
-              flexo.$h1("UBIK") + updates));
-        });
+      var uid = replies[0].uid;
+      var user = "%0 %1".fmt(replies[0].first, replies[0].last);
+      var info = flexo.$div({ "class": "user-info" },
+        flexo.$p(
+          flexo.$span({ "class": "uid" }, uid),
+          " (%0)".fmt(user)),
+        flexo.$p(
+          "Following: ",
+          replies[1].map(function (v) {
+            return flexo.$a({ href: "/user/%0".fmt(v)}, v);
+          }).join(", ") || "nobody"),
+        flexo.$p(
+          "Followers: ",
+          replies[2].map(function (v) {
+            return flexo.$a({ href: "/user/%0".fmt(v)}, v);
+          }).join(", ") || "none"));
+      var union = ["status", 1 + replies[1].length,
+          "user:%0:status".fmt(uid)];
+      replies[1].forEach(function (v) {
+        union.push("user:%0:status".fmt(v));
+      });
+      var m = redis.multi();
+      m.ZUNIONSTORE.apply(m, union);
+      m.ZREVRANGE("status", 0, -1).DEL("status")
+      .exec(function (err, replies) {
+        if (err) {
+          next(err);
+        } else {
+          var m = redis.multi();
+          replies[1].forEach(function (v) {
+            m.HGETALL("status:%0".fmt(v));
+          });
+          m.exec(function (err, replies) {
+            if (err) {
+              next(err);
+            } else {
+              var status = flexo.$ul({ "class": "status" },
+                replies.map(function (update) {
+                  return flexo.$li(
+                    flexo.$span({ "class": "uid" }, update.from),
+                    ": ",
+                    flexo.$span({ "class": "msg" }, "“%0”".fmt(update.msg)),
+                    " ",
+                    flexo.$span({ "class": "date" },
+                      new Date(parseFloat(update.date)).toString()));
+                }).join(""));
+              res.send(200, html({ title: "Ubik | %0".fmt(uid) },
+                  flexo.$link({ rel: "stylesheet", href: "/static/ubik.css" }),
+                  flexo.$h1("UBIK") + info + status));
+            }
+          });
+        }
+      });
     }
   });
 });
